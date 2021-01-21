e701a5f71977fcc7ea9173d1b0ffa859d2c521d8
arya <arya.prakyath@gmail.com>
2021-01-21 14:28:57 +0530

    Bug fixes

--- waterNotify.py
+++ waterNotify.py
@@ -1,7 +1,7 @@
 """
     Author: THE_ARYA
     Application Name:   Water Notification System
-    Desc:   This app notifies a 'drink water' reminder to the user every hour
+    Desc:   This app notifies a 'Drink Water' reminder to the user every hour
 """
 
 # External packages
@@ -9,46 +9,45 @@ from plyer import notification
 import time
 
 # main()
-if __name__ == "__main__":
-    # Initialize reminder counter for the day
-    counter = 1
+# Initialize reminder counter for the day
+counter = 1
 
-    # Initialize previous notification day
-    dayLast = 0
+# Initialize previous notification day
+dayLast = 0
 
-    # Initialize days list and dict
-    dayList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    daysDict = {x: y for y, x in enumerate(dayList)}
+# Initialize days list and dict
+dayList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+daysDict = {x: y for y, x in enumerate(dayList)}
 
-    # Run program forever until stopped
-    while True:
-        # Current time
-        timeStamp = time.asctime(time.localtime(time.time()))
+# Run program forever until stopped
+while True:
+    # Current time
+    timeStamp = time.asctime(time.localtime(time.time()))
 
-        # Current Day while notifying
-        dayNow = daysDict[timeStamp.split(' ')[0]]
+    # Current Day while notifying
+    dayNow = daysDict[timeStamp.split(' ')[0]]
 
-        # Time when notified
-        timeNow = timeStamp.split(' ')[3]
+    # Time when notified
+    timeNow = timeStamp.split(' ')[3]
 
-        # Reset counter if current notification day and previous notification day are different
-        if (dayNow > dayLast) or (dayLast == 6 and dayNow == 0):
-            counter = 1
+    # Reset counter if current notification day and previous notification day are different
+    if (dayNow > dayLast) or (dayLast == 6 and dayNow == 0):
+        counter = 1
 
-        # Deploy notification
-        notification.notify(title='Water Reminder: '+str(counter), message='Time to drink water...',
-                            app_icon='water-icon.ico', timeout=20)
+    # Deploy notification
+    notification.notify(title='Water Reminder: '+str(counter), message='Time to drink water...',
+                        app_icon='water-icon.ico', timeout=20)
 
-        # Update DataBase
-        with open("waterDB.txt", "a") as f:
-            f.write(f"Reminder No: {str(counter)} - {timeStamp.split(' ')[0]} @ {timeNow}\n")
-            f.close()
+    # Update DataBase
+    with open("waterDB.txt", "a") as f:
+        f.write(f"Reminder No: {str(counter)} - {timeStamp.split(' ')[0]} @ {timeNow}\n")
+        f.close()
 
-        # Increment counter
-        counter += 1
+    # Increment counter
+    counter += 1
 
-        # Set previous Notification day
-        dayLast = dayNow
+    # Set previous Notification day
+    dayLast = dayNow
 
-        # Wait for one hour until next notification deploy
-        time.sleep(3600)
+    # Wait for one hour until next notification deploy
+    time.sleep(3600)
